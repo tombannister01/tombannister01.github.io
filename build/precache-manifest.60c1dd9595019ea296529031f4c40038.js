408349224dcb4711b29ee4d77b59f611c82a3f7d
tombannister01 <tombannister2463@gmail.com>
2020-10-26 16:15:27 +0000

    adding build folder

---
+++ build/precache-manifest.60c1dd9595019ea296529031f4c40038.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770faa0004f62aab81f6945167f004a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76d86f82ae2c0c606191",
+    "url": "/static/css/2.c4f291ab.chunk.css"
+  },
+  {
+    "revision": "aa9298f42520ff14fb2f",
+    "url": "/static/css/main.2243402b.chunk.css"
+  },
+  {
+    "revision": "76d86f82ae2c0c606191",
+    "url": "/static/js/2.0a74b704.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0a74b704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9298f42520ff14fb2f",
+    "url": "/static/js/main.d614e153.chunk.js"
+  },
+  {
+    "revision": "0dfba102e0c69cb6ea6b",
+    "url": "/static/js/runtime-main.a3ab79ef.js"
+  },
+  {
+    "revision": "2ef0308dd6c4b1b1420167e066bf734b",
+    "url": "/static/media/Cities.2ef0308d.png"
+  },
+  {
+    "revision": "bf0bc76a18317b5d2681ff9fdaadf81c",
+    "url": "/static/media/FitnessTrackerApp.bf0bc76a.png"
+  },
+  {
+    "revision": "8782c719a22c712e9527d2a8b0a08813",
+    "url": "/static/media/Frogger.8782c719.png"
+  },
+  {
+    "revision": "bec47b059d1b3e242673125779c478e3",
+    "url": "/static/media/NutriFix.bec47b05.png"
+  },
+  {
+    "revision": "f424599ad4a87a3ea5c89b9a7e9be65e",
+    "url": "/static/media/ProfilePic.f424599a.jpg"
+  },
+  {
+    "revision": "c258f1ed3aac6c56e3f89eb45fc400b1",
+    "url": "/static/media/aboutme.c258f1ed.gif"
+  }
+]);
