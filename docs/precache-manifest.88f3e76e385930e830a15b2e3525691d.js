db7ff983d1b1bdf11213f843d765e0e87c313e33
tombannister01 <tombannister2463@gmail.com>
2020-10-26 16:52:54 +0000

    merging src and deploy into a ingle repo

---
+++ docs/precache-manifest.88f3e76e385930e830a15b2e3525691d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a895d3a47fe9d33a0b6ab2d97000b8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af48da17639c7eb32d34",
+    "url": "/static/css/2.11d66878.chunk.css"
+  },
+  {
+    "revision": "e36b9f9c3bdecf215056",
+    "url": "/static/css/main.2243402b.chunk.css"
+  },
+  {
+    "revision": "af48da17639c7eb32d34",
+    "url": "/static/js/2.e73bfb57.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e73bfb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36b9f9c3bdecf215056",
+    "url": "/static/js/main.40510f04.chunk.js"
+  },
+  {
+    "revision": "4c684c408e9f14b28ef8",
+    "url": "/static/js/runtime-main.d3643d34.js"
+  },
+  {
+    "revision": "2ef0308dd6c4b1b1420167e066bf734b",
+    "url": "/static/media/Cities.2ef0308d.png"
+  },
+  {
+    "revision": "bf0bc76a18317b5d2681ff9fdaadf81c",
+    "url": "/static/media/FitnessTrackerApp.bf0bc76a.png"
+  },
+  {
+    "revision": "8782c719a22c712e9527d2a8b0a08813",
+    "url": "/static/media/Frogger.8782c719.png"
+  },
+  {
+    "revision": "bec47b059d1b3e242673125779c478e3",
+    "url": "/static/media/NutriFix.bec47b05.png"
+  },
+  {
+    "revision": "f424599ad4a87a3ea5c89b9a7e9be65e",
+    "url": "/static/media/ProfilePic.f424599a.jpg"
+  },
+  {
+    "revision": "c258f1ed3aac6c56e3f89eb45fc400b1",
+    "url": "/static/media/aboutme.c258f1ed.gif"
+  }
+]);
---
+++ src/components/Project3.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Project3 = () => {
+  return <section className="section">
+    <div className="columns">
+
+      <div className="column">
+        <div className="card">
+          {/* <div className="card-header">
+        <h3 className="card-header-title">
+          
+        </h3>
+      </div> */}
+          <div className="card-image">
+            <figure className="image is-4by3 project-3">
+            </figure>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="column">
+        <div className="card">
+
+          <div className="card-header">
+            <h3 className="card-header-title">
+              NutriFix
+          </h3>
+          </div>
+          <div className="card-content">
+            <strong> Project 3 </strong> - A Full-stack recipe app which would allow you to find new recipes or build your own.I was in a group of 4 and we had around 1 week to complete this project. We used technologies such as: Mongo, Express, React and Node.js (MERN) to create this app. I was responsible for functionalities on the Front-end such as tag selection, displaying the recipes, app routing etc. I was also involved in processes on the back-end which used a Model View Controller (MVC) approach. We worked well as a group and delegated individual/paired work to be completed throughout the day which turned out to be an efficient way of delegating the work-load.
+            <br></br>
+            <small>http://nutrifix2.herokuapp.com/</small>
+        </div>
+
+        </div>
+        <div className="skills-container1">
+          <div className="skills-container-title">
+            Technologies used
+        </div>
+          <div className="icon-container">
+            <i className="devicon-css3-plain-wordmark"></i>
+            <i className="devicon-git-plain"></i>
+            <i className="devicon-github-plain"></i>
+            <i className="devicon-html5-plain-wordmark"></i>
+            <i className="devicon-javascript-plain"></i>
+            <i className="devicon-visualstudio-plain"></i>
+            <i className="devicon-react-original-wordmark"></i>
+            <i className="devicon-sass-original"></i>
+            <i className="devicon-express-original"></i>
+            <i className="devicon-heroku-plain-wordmark"></i>
+            <i className="devicon-mongodb-plain-wordmark"></i>
+            <i className="devicon-nodejs-plain"></i>
+            <i className="devicon-trello-plain-wordmark"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+}
+
+export default Project3
